98e75c403f5ee8a2bebc61b2c7602328f4048461
John Francis <john1jan@gmail.com>
2016-04-28 15:35:51 +0530

    Added new files

---
+++ index.android.js
@@ -0,0 +1,88 @@
+    /**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    View, TouchableHighlight
+} from 'react-native';
+
+import SignatureCapture from 'react-native-signature-capture';
+
+
+
+class SignaturExample extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, flexDirection: "column" }}>
+                <Text style={{alignItems:"center",justifyContent:"center"}}>Signature Capture Extended </Text>
+                <SignatureCapture
+                    style={[{flex:1},styles.signature]}
+                    ref="sign"
+                    onSaveEvent={this._onSaveEvent}
+                    onDragEvent={this._onDragEvent}
+                    saveImageFileInExtStorage={false}
+                    showNativeButtons={false}
+                    viewMode={"portrait"}/>
+
+                <View style={{ flex: 1, flexDirection: "row" }}>
+                    <TouchableHighlight style={styles.buttonStyle}
+                        onPress={() => { this.saveSign() } } >
+                        <Text>Save</Text>
+                    </TouchableHighlight>
+
+                    <TouchableHighlight style={styles.buttonStyle}
+                        onPress={() => { this.resetSign() } } >
+                        <Text>Reset</Text>
+                    </TouchableHighlight>
+
+                </View>
+
+            </View>
+
+
+
+        );
+    }
+
+    saveSign() {
+        this.refs["sign"].saveImage();
+    }
+
+    resetSign() {
+        this.refs["sign"].resetImage();
+    }
+
+    _onSaveEvent(result) {
+        //result.encoded - for the base64 encoded png 
+        //result.pathName - for the file path name 
+        console.log(result);
+    }
+    _onDragEvent() {
+         // This callback will be called when the user enters signature
+        console.log("dragged");
+    }
+}
+
+
+
+const styles = StyleSheet.create({
+
+    signature: {
+        flex: 1,
+        borderColor: '#000033',
+        borderWidth: 1,
+    },
+    buttonStyle: {
+        flex: 1, justifyContent: "center", alignItems: "center", height: 50,
+        backgroundColor: "#eeeeee",
+        margin: 10
+        
+    }
+});
+
+AppRegistry.registerComponent('SignaturExample', () => SignaturExample);
